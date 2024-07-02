dba97ac4fdef7c20f7edb256deb74af3a57893d9
Daniel Golliher <danielgolliher@daniels-air-4.mynetworksettings.com>
2024-07-02 19:47:45 -0400

    update

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const port = process.env.PORT || 3000
 app.use(express.static("public"))
 
 app.get("/", (req, res) => {
-    const filepath = __dirname + "/public/index.html"
+    const filepath = '/opt/render/project/src/public/index.html'
+    //    const filepath = __dirname + "/public/index.html"
     console.log(filepath)
     console.log("hello from inside the index route")
     res.sendFile(filepath)
